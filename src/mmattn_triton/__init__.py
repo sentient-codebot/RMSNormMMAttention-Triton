2d6e67e789b4f703b5b2a3009747f4778c0bd4d9
nan <nanlin1997@outlook.com>
2025-12-10 14:25:29 +0100

    chore: update package exports with new kernels and modules

--- src/mmattn_triton/__init__.py
+++ src/mmattn_triton/__init__.py
@@ -1,2 +1,11 @@
-def hello() -> str:
-    return "Hello from rmsnormmmattention-triton!"
+from .fused_rmsnorm import rmsnorm_adaln
+from .fused_layernorm import layernorm_adaln
+from .nn_torch import GPT2Block, RMSNormSelfAttention, RMSNormSelfCrossAttention
+
+__all__ = [
+    "rmsnorm_adaln",
+    "layernorm_adaln",
+    "GPT2Block",
+    "RMSNormSelfAttention",
+    "RMSNormSelfCrossAttention",
+]
